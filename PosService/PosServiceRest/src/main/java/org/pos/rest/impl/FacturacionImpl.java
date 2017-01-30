535e046a33c269252d24592570c346e531c06312
Camilo <cbustamantes@ucentral.edu.co>
2017-01-30 00:13:42 -0500

    Diagramas...

--- PosService/PosServiceRest/src/main/java/org/pos/rest/impl/FacturacionImpl.java
+++ PosService/PosServiceRest/src/main/java/org/pos/rest/impl/FacturacionImpl.java
@@ -4,7 +4,10 @@ import javax.ws.rs.core.Response;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.pos.core.controller.MovimientoController;
 import org.pos.core.dto.FacturacionDto;
+import org.pos.core.dto.MsgResponseDto;
+import org.pos.db.entidades.Movimiento;
 import org.pos.rest.service.FacturacionService;
 
 public class FacturacionImpl implements FacturacionService{
@@ -13,15 +16,38 @@ public class FacturacionImpl implements FacturacionService{
 
 	@Override
 	public Response consultaVenta(FacturacionDto fact) {
+		
+		Movimiento movimiento = new Movimiento();
 		// TODO Auto-generated method stub
-		log.info("");
-		return null;
+		
+		log.info("Registrando Ingreso Producto");		
+		
+		MsgResponseDto response = new MovimientoController().registraIngreso(movimiento);
+
+		if (response != null) {
+			return Response.status(200).entity(response).build();
+		} else {
+			log.warn("No fue posible realizar el Ingreso de los Productos");
+			return Response.status(200).entity(null).build();
+		}
+		
 	}
 
 	@Override
 	public Response registraVenta(FacturacionDto fact) {
+		Movimiento movimiento = new Movimiento();
 		// TODO Auto-generated method stub
-		return null;
+		
+		log.info("Registrando Ingreso Producto");		
+		
+		MsgResponseDto response = new MovimientoController().registraIngreso(movimiento);
+
+		if (response != null) {
+			return Response.status(200).entity(response).build();
+		} else {
+			log.warn("No fue posible realizar el Ingreso de los Productos");
+			return Response.status(200).entity(null).build();
+		}
 	}
 
 	
